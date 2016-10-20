a67005dd2d0c6470d1db9b3f9301678ff56acd7a
GeoffroyBerry <berrygeoffroy@gmail.com>
2016-10-20 14:22:28 +0200

    Ajout du code projet pour eclipse

---
+++ ProjetEclipse/Syracus/src/p1/main.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Fenetre f = new Fenetre();
+
+	}
+
+}
